43b4612c01b0b2525bf8d4162b5591c67cd2afb2
Danielle.Leblanc-Cyr <danielleleb12@gmail.com>
2018-02-26 17:29:52 +0100

    setup app

---
+++ app.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+
+const index = require('./routes/index');
+
+const app = express();
+
+
+app.use(cors({
+  credentials: true,
+  origin: ['http://localhost:4200']
+}));
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', index);
+
+// -- error handlers
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  res.status(404).json({error: 'not found'});
+});
+
+app.use((err, req, res, next) => {
+  // always log the error
+  console.error('ERROR', req.method, req.path, err);
+
+  // only render if the error ocurred before sending the response
+  if (!res.headersSent) {
+    res.status(500).json({error: 'unexpected'});
+  }
+});
+module.exports = app;
